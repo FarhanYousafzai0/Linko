bd57ecd96a333109ee0bc9bae87a00c128aee1da
Farhan Yousafzai <coderwithferry@gmail.com>
2025-08-20 23:33:26 +0500

    Jwt

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -6,11 +6,7 @@ export const connectDB = async () => {
   if (isConnected) return;
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "Link0",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     isConnected = true;
     console.log("✅ MongoDB connected");
   } catch (err) {
